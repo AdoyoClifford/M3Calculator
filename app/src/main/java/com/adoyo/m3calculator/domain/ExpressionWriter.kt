167effa62979528ecb57f5acfdce6a8e619357a8
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-23 23:03:14 +0300

    initial commit

--- app/src/main/java/com/adoyo/m3calculator/domain/ExpressionWriter.kt
+++ app/src/main/java/com/adoyo/m3calculator/domain/ExpressionWriter.kt
@@ -3,6 +3,44 @@ package com.adoyo.m3calculator.domain
 class ExpressionWriter {
     var expression = ""
 
+    fun processAction(action: CalculatorActions) {
+        when (action) {
+            CalculatorActions.Calculate -> {
+                val parser = ExpressionParser(prepareCalculation())
+                val evaluator = ExpressionEvaluator(parser.parse())
+                expression = evaluator.evaluate().toString()
+            }
+
+            CalculatorActions.Clear -> {
+                expression = ""
+            }
+
+            CalculatorActions.Decimal -> {
+                if (canEnterDecimal()) {
+                    expression += "."
+                }
+            }
+
+            CalculatorActions.Delete -> {
+                expression = expression.dropLast(1)
+            }
+
+            is CalculatorActions.Number -> {
+                expression += action.number
+            }
+
+            is CalculatorActions.Operation -> {
+                if (canEnterOperation(action.operations)) {
+                    expression += action.operations.symbol
+                }
+            }
+
+            CalculatorActions.Parenthesis -> {
+                processParenthesis()
+            }
+        }
+    }
+
     private fun canEnterOperation(operation: Operations): Boolean {
         if (operation in listOf(Operations.ADD, Operations.SUBTRACT)) {
             return expression.isEmpty() || expression.last() in "$operationSymbol()0123456789"
@@ -10,7 +48,7 @@ class ExpressionWriter {
         return expression.isNotEmpty() || expression.last() in "0123456789)"
     }
 
-    private fun canEnterDecimal(operation: Operations): Boolean {
+    private fun canEnterDecimal(): Boolean {
         if (expression.isEmpty() || expression.last() in "$operationSymbol.()") {
             return false
         }
@@ -21,11 +59,11 @@ class ExpressionWriter {
     }
 
     private fun processParenthesis() {
-        val openingCount = expression.count {it == '('}
-        val closingCount = expression.count {it == ')'}
+        val openingCount = expression.count { it == '(' }
+        val closingCount = expression.count { it == ')' }
         expression += when {
-            expression.isEmpty() || expression.last() in "$operationSymbol("->"("
-                    expression.last() in "0123456789)" && openingCount == closingCount -> return
+            expression.isEmpty() || expression.last() in "$operationSymbol(" -> "("
+            expression.last() in "0123456789)" && openingCount == closingCount -> return
             else -> ")"
         }
     }
@@ -34,7 +72,7 @@ class ExpressionWriter {
         val newExpression = expression.dropLastWhile {
             it in "$operationSymbol(."
         }
-        if(newExpression.isEmpty()) {
+        if (newExpression.isEmpty()) {
             return "0"
         }
         return newExpression
